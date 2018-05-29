ddc9a34eb305bcf5ac930d4ec1ffa50a36a8a2cb
Bryan Venteicher <bryanventeicher@gmail.com>
2018-05-29 00:54:36 -0500

    if_vtnet: Rework r303971 max multiqueue pairs check
    The changes in r303971 did not check the tunable max pairs value
    against the device's max_virtqueue_pairs (only the spec max) so
    it was possible to attempt to use more queues than available.
    
    Verify the max_virtqueue_pairs is within the range allowed by
    the spec.

--- sys/dev/virtio/network/if_vtnetvar.h
+++ sys/dev/virtio/network/if_vtnetvar.h
@@ -159,8 +159,8 @@ struct vtnet_softc {
 	int			 vtnet_if_flags;
 	int			 vtnet_max_mtu;
 	int			 vtnet_act_vq_pairs;
+	int			 vtnet_req_vq_pairs;
 	int			 vtnet_max_vq_pairs;
-	int			 vtnet_requested_vq_pairs;
 
 	struct virtqueue	*vtnet_ctrl_vq;
 	struct vtnet_mac_filter	*vtnet_mac_filter;
