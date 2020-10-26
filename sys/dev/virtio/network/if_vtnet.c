59d0145143f077e05b62b3576fa862583362f191
Bryan Venteicher <bryanventeicher@gmail.com>
2020-10-26 04:35:39 +0000

    Revert "vtnet: Fix regression introduced in r361944"
    This reverts commit ad49a04033c58990c7c9f23e87ea8099b0f8ca48.

--- sys/dev/virtio/network/if_vtnet.c
+++ sys/dev/virtio/network/if_vtnet.c
@@ -1819,10 +1819,9 @@ vtnet_rxq_eof(struct vtnet_rxq *rxq)
 			adjsz = sizeof(struct vtnet_rx_header);
 			/*
 			 * Account for our pad inserted between the header
-			 * and the actual start of the frame. This includes
-			 * the unused num_buffers when using a legacy device.
+			 * and the actual start of the frame.
 			 */
-			len += adjsz - sc->vtnet_hdr_size;
+			len += VTNET_RX_HEADER_PAD;
 		} else {
 			mhdr = mtod(m, struct virtio_net_hdr_mrg_rxbuf *);
 			nbufs = mhdr->num_buffers;
